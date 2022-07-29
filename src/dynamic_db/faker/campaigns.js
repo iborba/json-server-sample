0da31e7d51ce60fd2804d91167a31d8d9107c9bf
Israel Borba <israel.borba@gmail.com>
2022-07-29 10:30:21 -0300

    implement faker for campaigns

--- src/dynamic_db/faker/campaigns.js
+++ src/dynamic_db/faker/campaigns.js
@@ -1,12 +1,19 @@
 const Chance = require('chance')
 
-module.exports = function analysis() {
+module.exports = function campaigns() {
   const chance = new Chance()
-  const data = { analysis: [] }
+  const data = { campaigns: [] }
 
   for (let i = 0; i < 10; i++) {
-    data.analysis.push({
-
+    data.campaigns.push({
+      campaignId: chance.integer(), //TODO refactor
+      campaignDs: chance.word(),
+      beginDt: chance.timestamp(), //TODO refactor
+      endDt: chance.timestamp(), //TODO refactor
+      createdAt: chance.timestamp(), //TODO refactor
+      updatedAt: chance.timestamp(), //TODO refactor
+      createdBy: chance.first(),
+      status: chance.bool()
     })
   }
 
